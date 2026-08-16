7100ab53b340eea21b2fec9fb50fd4b71217337f
Paul Tan <64180059+paulhk88@users.noreply.github.com>
2026-08-16 20:33:28 +0800

    Integrate Twilio for weather notifications
    Refactor to use Twilio for notifications and remove email functionality.

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+# main-27.py
+"""
+task-276-Scheduling to trigger via GitHub
+"""
+import requests
+from twilio.rest import Client
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+TWILIO_ACCOUNT_SID = os.environ.get("ACCT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
+MY_LAT = 4.210484
+MY_LNG = 101.975769
+MY_API = os.environ.get("OWM_API_KEY")
+API_END_POINT = "https://api.openweathermap.org/data/2.5/forecast"
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+# add the "cnt" parameter o limit the number of items in the response
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LNG,
+    "appid": MY_API,
+    "cnt": 4,
+}
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+response = requests.get(API_END_POINT, params=parameters)
+response.raise_for_status()
+weather_data = response.json()
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+# print(weather_data["list"][0]["weather"][0]["id"])
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    account_sid = TWILIO_ACCOUNT_SID
+    auth_token = TWILIO_AUTH_TOKEN
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+6596690168",
+        body="Local-It is going to rain today. Remember to bring an umbrella."
+    )
+
+    print(message.body)
